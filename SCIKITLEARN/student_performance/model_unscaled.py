aef39ad81867e21908128b3b14f6668e306538f7
Jemelrey Abastillas <jemelreyabastillas@gmail.com>
2026-07-14 18:16:46 +0800

    Add linear regression model for student performance
    This script loads student performance data, preprocesses it, trains a linear regression model, and evaluates its performance metrics. It also makes predictions for a new student's final exam score based on input features.

---
+++ SCIKITLEARN/student_performance/model_unscaled.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.preprocessing import LabelEncoder
+
+
+df = pd.read_csv("student_performance.csv")
+fill_col = ["StudyHours" , "Attendance", "SleepHours", "AssignmentsCompleted", "PreviousGrade",]
+for col in fill_col:
+    df[col] = df[col].fillna(df[col].median()).abs()
+df["InternetAccess"] = df["InternetAccess"].fillna(df["InternetAccess"].mode()[0]).str.strip().str.lower()
+le = LabelEncoder()
+
+df["InternetAccess"] = le.fit_transform(df["InternetAccess"])
+X = df[["StudyHours", "Attendance", "SleepHours", "AssignmentsCompleted", "PreviousGrade", "InternetAccess"]]
+y = df["FinalExamScore"]
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+model = LinearRegression()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+
+print("\nMETRICS")
+print(mean_absolute_error(y_test,predictions))
+print(mean_squared_error(y_test, predictions))
+print(r2_score(y_test, predictions))
+
+coef = pd.DataFrame({
+    "Feature" : X.columns,
+    "Weights" : model.coef_
+})
+print("\nCoefficients")
+print(coef)
+
+comparison = pd.DataFrame({
+    "Actual" : y_test.values,
+    "Predicted" : predictions.round(2)
+})
+print(comparison)
+print(df)
+new_student = pd.DataFrame({
+    "StudyHours" : [5.0],
+    "Attendance" : [67],
+    "SleepHours" : [6],
+    "AssignmentsCompleted"  : [10],
+    "PreviousGrade" : [87],
+    "InternetAccess" : [0]
+})
+new_prediction = model.predict(new_student)
+print("FinalExamScore",new_prediction)
